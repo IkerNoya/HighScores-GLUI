6d8533dc716fb91f3cb4c15e7a87710a1780d2e2
Iker <ikernoya4@gmail.com>
2020-05-01 21:37:52 -0300

    Created a window with an empty scores table

--- Scores-GLUI/main.cpp
+++ Scores-GLUI/main.cpp
@@ -1,4 +1,72 @@
+#include <iostream>
+#include <stdlib.h>
+
 #include "GL/glui.h"
 #include "GL/glut.h"
 #include "HighScore.h"
+
+using namespace std;
+using namespace HighScoreListLT;
+
+const int maxL = 26;
+
+void display();
+bool initGL();
+
+int main_window;
+
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	if (!initGL())
+	{
+		exit(1);
+	}
+	glutMainLoop();
+	return 0;
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_QUADS);
+	//------------green---------------
+	glColor3f(0, 0.6f, 0.15f);
+	glVertex2f(-0.5f, 0.8f); // top left
+	glVertex2f(0.5f, 0.8f); // top right 
+	glVertex2f(0.5f, -0.4f); // bottom right
+	glVertex2f(-0.5f, -0.4f); // bottom left
+	//------------white----------------
+	glColor3f(1, 1, 1);
+	glVertex2f(-0.5f, 0.8f); // top left
+	glVertex2f(0.5f, 0.8f); // top right 
+	glVertex2f(0.5f, 0.57f); // bottom right
+	glVertex2f(-0.5f, 0.57f); // bottom left
+
+	glEnd();
+
+	glColor3f(0, 0, 0);
+
+	glutSwapBuffers();
+}
+
+bool initGL()
+{
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	main_window = glutCreateWindow("HighScores");
+	glClearColor(0, 0, 0, 1);
+	glutDisplayFunc(display);
+
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR)
+	{
+		cout << "error de inicializacion" << gluErrorString(error) << endl;
+		return false;
+	}
+	return true;
+}
 
